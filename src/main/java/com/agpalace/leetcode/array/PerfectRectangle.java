c032558ed10f66370d41b57c66c1693c30459014
邓明 <dengming@dengmingdeMacBook-Pro.local>
2016-09-02 21:30:48 +0800

    finish the PerfectRectangle, but do not test

--- src/main/java/com/agpalace/leetcode/array/PerfectRectangle.java
+++ src/main/java/com/agpalace/leetcode/array/PerfectRectangle.java
@@ -5,36 +5,93 @@ package com.agpalace.leetcode.array;
  * question url:https://leetcode.com/problems/perfect-rectangle/
  */
 public class PerfectRectangle {
+    private boolean[][] region;
+    private int[] leftBtm;
+    private int[] rightTop;
 
     public boolean isRectangleCover(int[][] rectangles) {
-        if (correectArea(rectangles)){
-            //TODO
+        init(rectangles);
+        for (int[] rec : rectangles) {
+            if (!coverSquare(rec)) {
+                return false;
+            }
+        }
+
+        return validatedSquare();
+    }
+
+    private boolean validatedSquare() {
+        for (boolean[] a : region) {
+            for (boolean b : a) {
+                if (!b) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void init(int[][] rectangles) {
+        initSquare(rectangles);
+        region = new boolean[rightTop[2] - leftBtm[0]][rightTop[3] - leftBtm[1]];
+    }
+
+    private void initSquare(int[][] rectangles) {
+        int minX = rectangles[0][0];
+        int minY = rectangles[0][1];
+        int maxX = rectangles[0][2];
+        int maxY = rectangles[0][3];
+        for (int i = 0; i < rectangles.length; i++) {
+            int[] rec = rectangles[i];
+            if (minX >= rec[0] && minY >= rec[1]) {
+                minX = rec[0];
+                minY = rec[1];
+                leftBtm = rec;
+            }
+
+            if (maxX <= rec[2] && maxY <= rec[3]) {
+                maxX = rec[2];
+                maxY = rec[3];
+                rightTop = rec;
+            }
+        }
+    }
+
+    private boolean coverSquare(int[] rect) {
+        if (validatedRec(rect)) {
+            int[] relatedIndex = translate(rect);
+            return cover(relatedIndex);
         }
         return false;
     }
 
-    //whether the total areas can be square
-    private boolean correectArea(int[][] rectangles){
-        int area=calcaulateTotalArea(rectangles);
-        return isSquare(area);
-    }
-
-    private boolean isSquare(int area) {
-        for (int i=1; area>0; i+=2){
-            area-=i;
+    private boolean cover(int[] relatedIndex) {
+        for (int i = relatedIndex[0]; i < relatedIndex[2]; i++) {
+            for (int j = relatedIndex[1]; j < relatedIndex[3]; j++) {
+                if (region[i][j]) {
+                    return false;
+                } else {
+                    region[i][j] = true;
+                }
+            }
         }
-        return area==0;
+        return true;
     }
 
-    private int calcaulateTotalArea(int[][] rectangles) {
-        int total=0;
-        for (int[] rectl: rectangles){
-            total+=calcaulateArea(rectl);
-        }
-        return total;
+    //using the leftBtm as the origin point
+    private int[] translate(int[] rect) {
+        int[] res = new int[rect.length];
+        res[0] = rect[0] - leftBtm[0];
+        res[1] = rect[1] - leftBtm[1];
+        res[2] = rect[2] - leftBtm[0];
+        res[3] = rect[3] - leftBtm[1];
+        return res;
     }
 
-    private int calcaulateArea(int[] rectl) {
-        return (rectl[3]-rectl[1])*(rectl[2]-rectl[0]);
+    private boolean validatedRec(int[] rect) {
+        return rect[0] >= leftBtm[0]
+                && rect[1] >= leftBtm[1]
+                && rect[2] <= rightTop[2]
+                && rect[3] <= rightTop[3];
     }
 }
--- src/test/java/com/agpalace/leetcode/array/PerfectRectangleTest.java
+++ src/test/java/com/agpalace/leetcode/array/PerfectRectangleTest.java
@@ -54,5 +54,25 @@ public class PerfectRectangleTest {
         };
         Assert.assertTrue(!rectangle.isRectangleCover(array));
     }
+    
+    @Test
+    public void test5(){
+        int [][] array={
+                {0,0,4,1},
+                {7,0,8,2},
+                {6,2,8,3},
+                {5,1,6,3},
+                {4,0,5,1},
+                {6,0,7,2},
+                {4,2,5,3},
+                {2,1,4,3},
+                {0,1,2,2},
+                {0,2,2,3},
+                {4,1,5,2},
+                {5,0,6,1}
+        };
+        Assert.assertTrue(!rectangle.isRectangleCover(array));
+    }
+
 
 }
