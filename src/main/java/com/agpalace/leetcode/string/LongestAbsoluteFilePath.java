42c624847f2189de4f389942f8f205236e82e597
flycash <mingflycash@gmail.com>
2016-09-05 20:51:29 +0800

    Longest Absolute File Path, a wrong solution

--- src/main/java/com/agpalace/leetcode/string/LongestAbsoluteFilePath.java
+++ src/main/java/com/agpalace/leetcode/string/LongestAbsoluteFilePath.java
@@ -1,12 +1,102 @@
 package com.agpalace.leetcode.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by dengming on 16/9/4.
  * https://leetcode.com/problems/longest-absolute-file-path/
  */
 public class LongestAbsoluteFilePath {
 
+    private Map<Integer, Integer> longest = new HashMap<Integer, Integer>();
+    private int tNum = 0;
+    private StringBuilder next = new StringBuilder();
+    private int max = 0;
+    private int lastTNum = Integer.MIN_VALUE;
+
     public int lengthLongestPath(String input) {
-        return 0;
+        int index = input.length() - 1;
+        while (index >= 0) {
+            int nextN = findNextN(input, index);
+            if (isFile()) {
+                setLength(tNum, max(longest.get(tNum), next.length()));
+                lastTNum = tNum;
+            } else {
+                handleDir();
+            }
+            index = nextN - 1;
+        }
+        max = max(longest.get(0), max);
+        return max;
+    }
+
+    private void handleDir() {
+
+        Integer downLevel = longest.get(tNum + 1);
+        Integer old = longest.get(tNum);
+        if (tNum+1!=lastTNum){
+            downLevel=0;
+        }
+        if (downLevel == null || downLevel == 0) {
+            int tmp = old == null ? 0 : old;
+            longest.put(tNum, tmp);
+        } else {
+            int newL = next.length() + downLevel + 1;
+            if (old == null || old == 0) {
+                longest.put(tNum, newL);
+            } else {
+                if (newL > old) {
+                    longest.put(tNum, newL);
+                }
+            }
+        }
+
+        lastTNum = tNum;
+        if (tNum == 0) {
+            clear();
+        }
+    }
+
+    private void clear() {
+        Integer new0 = longest.get(0);
+        max = max(new0, max);
+        longest.clear();
+    }
+
+    private boolean isFile() {
+        return this.next.indexOf(".") != -1;
+    }
+
+    private void setLength(int index, int length) {
+        Integer origin = longest.get(index);
+        if (origin == null) {
+            longest.put(index, length);
+        } else {
+            int l = length > origin ? length : origin;
+            longest.put(index, l);
+        }
+    }
+
+    private int findNextN(String input, int index) {
+        next.delete(0, next.length());
+        tNum = 0;
+        while (index >= 0) {
+            char c = input.charAt(index);
+            if (c == '\n') {
+                break;
+            } else if (c == '\t') {
+                tNum++;
+            } else {
+                next.append(c);
+            }
+            index--;
+        }
+        return index;
+    }
+
+    private int max(Integer a, int b) {
+        int a1 = a == null ? 0 : a;
+        return Math.max(a1, b);
     }
 }
--- src/test/java/com/agpalace/leetcode/string/LongestAbsoluteFilePathTest.java
+++ src/test/java/com/agpalace/leetcode/string/LongestAbsoluteFilePathTest.java
@@ -1,5 +1,6 @@
 package com.agpalace.leetcode.string;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -9,7 +10,84 @@ public class LongestAbsoluteFilePathTest {
     private LongestAbsoluteFilePath path=new LongestAbsoluteFilePath();
     @Test
     public void test(){
-        String s="dir\\n\\tsubdir1\\n\\tsubdir2\\n\\t\\tfile.ext";
+        String s="dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext";
+        t(20, s);
     }
 
+    @Test
+    public void test1(){
+        String s="dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+        Assert.assertTrue(path.lengthLongestPath(s)==32);
+    }
+
+    @Test
+    public void test2(){
+        String s="";
+        Assert.assertTrue(path.lengthLongestPath(s)==0);
+    }
+
+    @Test
+    public void test3(){
+        String s="a";
+        int res=path.lengthLongestPath(s);
+        System.out.println("res:"+res);
+        Assert.assertTrue(res==0);
+    }
+
+    @Test
+    public void test4(){
+        String s="a.b";
+        int res=path.lengthLongestPath(s);
+        System.out.println("res:"+res);
+        Assert.assertTrue(res==3);
+    }
+
+    @Test
+    public void test5(){
+        String s="a\n\tb\n\t\tc.txt";
+        t(9, s);
+    }
+
+    @Test
+    public void test6(){
+        t(12, "dir\n    file.txt");
+    }
+
+    @Test
+    public void test7(){
+        t(47, "rzzmf\nv\n\tix\n\t\tiklav\n\t\t\ttqse\n\t\t\t\ttppzf\n\t\t\t\t\tzav\n\t\t\t\t\t\tkktei\n\t\t\t\t\t\t\thhmav\n\t\t\t\t\t\t\t\tbzvwf.txt");
+    }
+
+    @Test
+    public void test8(){
+        t(528, "sladjf\n\tlkjlkv\n\t\tlkjlakjlert" +
+                "\n\t\t\tlaskjglaksjf\n\t\t\t\tlakjgfljrtlj\n\t\t\t\t\tlskajflakjsvlj" +
+                "\n\t\t\t\t\t\tlskgjflkjrtlrjt\n\t\t\t\t\t\t\tlkjglkjlvkjdlvkj\n\t\t\t\t\t\t\t\tlfjkglkjfljdlv" +
+                "\n\t\t\t\t\t\t\t\t\tlkdfjglerjtkrjkljsd.lkvjlkajlfk" +
+                "\n\t\t\t\t\t\t\tlskfjlksjljslvjxjlvkzjljajoiwjejlskjslfj.slkjflskjldfkjoietruioskljfkljf" +
+                "\n\t\t\t\t\tlkasjfljsaljlxkcjzljvl.asljlksaj\n\t\t\t\tasldjflksajf\n\t\t\t\talskjflkasjlvkja" +
+                "\n\t\t\t\twioeuoiwutrljsgfjlskfg\n\t\t\t\taslkjvlksjvlkjsflgj\n\t\t\t\t\tlkvnlksfgk.salfkjaslfjskljfv" +
+                "\n\t\t\tlksdjflsajlkfj\n\t\t\tlasjflaskjlk\n\t\tlsakjflkasjfkljas\n\t\tlskjvljvlkjlsjfkgljfg" +
+                "\n\tsaljkglksajvlkjvkljlkjvksdj\n\tlsakjglksajkvjlkjdklvj\n\tlskjflksjglkdjbkljdbkjslkj\n\t\tlkjglkfjkljsdflj" +
+                "\n\t\t\tlskjfglkjdfgkljsdflj\n\t\t\t\tlkfjglksdjlkjbsdlkjbk\n\t\t\t\t\tlkfgjlejrtljkljsdflgjl" +
+                "\n\t\t\t\t\tsalgkfjlksfjgkljsgfjl\n\t\t\t\t\tsalkflajwoieu\n\t\t\t\t\t\tlaskjfglsjfgljkkvjsdlkjbklds" +
+                "\n\t\t\t\t\t\t\tlasjglriotuojgkjsldfgjsklfgjl\n\t\t\t\t\t\t\t\tlkajglkjskljsdljblkdfjblfjlbjs" +
+                "\n\t\t\t\t\t\t\t\t\tlkajgljroituksfglkjslkjgoi\n\t\t\t\t\t\t\t\t\t\tlkjglkjkljkljdkbljsdfljgklfdj" +
+                "\n\t\t\t\t\t\t\t\t\t\t\tlkjlgkjljgslkdkldjblkj\n\t\t\t\t\t\t\t\t\t\t\t\tlkjfglkjlkjbsdklj.slgfjalksjglkfjglf" +
+                "\n\t\t\t\t\t\t\t\t\t\t\t\tlkasjrlkjwlrjljsl\n\t\t\t\t\t\t\t\t\t\t\t\t\tlksjgflkjfklgjljbljls" +
+                "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjsglkjlkjfkljdklbjkldf\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjglkjdlsfjdglsdjgjlxljjlrjsgjsjlk" +
+                "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjsgkllksjfgjljdslfkjlkasjdflkjxcljvlkjsgkljsfg" +
+                "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlaskjlkjsakljglsdjfgksdjlkgjdlskjb" +
+                "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkajsgfljfklgjlkdjgfklsdjklj" +
+                "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjfglkjlkgjlkjl.aslkjflasjlajglkjaf" +
+                "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjasflgjlskjglkfjgklgsdjflkbjsdklfjskldfjgklsfdjgklfdjgl" +
+                "\n\tlskadjlkjsldwwwwwfj" +
+                "\n\t\tlkjflkasjlfjlkjajslfkjlasjkdlfjlaskjalvwwwwwwwwwwwwwwwkjlsjfglkjalsjgflkjaljlkdsjslbjsljksldjlsjdlkjljvblkjlkajfljgasljfkajgfljfjgldjblkjsdljgsldjg.skljf");
+    }
+
+    private void t(int target, String input){
+        int res=path.lengthLongestPath(input);
+        System.out.println("res:"+res);
+        Assert.assertTrue(res==target);
+    }
 }
