2580f2bb1168386512f76dc0d113682eaa293e33
flycash <mingflycash@gmail.com>
2016-09-06 14:44:32 +0800

    finish LongestAbsoluteFilePath

--- src/main/java/com/agpalace/leetcode/string/LongestAbsoluteFilePath.java
+++ src/main/java/com/agpalace/leetcode/string/LongestAbsoluteFilePath.java
@@ -12,81 +12,67 @@ public class LongestAbsoluteFilePath {
     private Map<Integer, Integer> longest = new HashMap<Integer, Integer>();
     private int tNum = 0;
     private StringBuilder next = new StringBuilder();
-    private int max = 0;
     private int lastTNum = Integer.MIN_VALUE;
+    private int max;
 
     public int lengthLongestPath(String input) {
         int index = input.length() - 1;
         while (index >= 0) {
             int nextN = findNextN(input, index);
+            if (tNum + 1 != lastTNum) {
+                clear(lastTNum+1);
+            }
             if (isFile()) {
-                setLength(tNum, max(longest.get(tNum), next.length()));
-                lastTNum = tNum;
+                handleFile();
             } else {
                 handleDir();
             }
+
+            if (tNum==0){
+                max=Math.max(get(0), max);
+            }
+
             index = nextN - 1;
         }
-        max = max(longest.get(0), max);
         return max;
     }
 
-    private void handleDir() {
-        if (tNum+1!=lastTNum){
-            clear(tNum+1);
-        }
-        Integer downLevel = longest.get(tNum + 1);
-        Integer old = longest.get(tNum);
-        if (downLevel == null || downLevel == 0) {
-            int tmp = old == null ? 0 : old;
-            longest.put(tNum, tmp);
-        } else {
-            int newL = next.length() + downLevel + 1;
-            if (old == null || old == 0) {
-                longest.put(tNum, newL);
-            } else {
-                if (newL > old) {
-                    longest.put(tNum, newL);
-                }
-            }
-        }
-
+    private void handleFile() {
+        int old=get(tNum);
+        longest.put(tNum, Math.max(old, next.length()));
         lastTNum = tNum;
-        if (tNum == 0) {
-            clear();
-        }
     }
 
-    private void clear(int i) {
-        for (Integer key:longest.keySet()){
-            if (key>=i){
+    private void handleDir() {
+
+        int downLevel = get(tNum + 1);
+        int old = get(tNum);
+        if (downLevel == 0) {
+            longest.put(tNum, Math.max(old, 0));
+        } else {
+            int newL = next.length() + downLevel + 1;
+            if (newL > old) {
+                longest.put(tNum, newL);
+            }
+
+        }
+        lastTNum = tNum;
+    }
+
+    private void clear(Integer start){
+        for (Integer key: longest.keySet()){
+            if (key>=start){
                 longest.put(key, 0);
             }
         }
-    }
-
-    private void clear() {
-        Integer new0 = longest.get(0);
-        max = max(new0, max);
-        longest.clear();
     }
 
     private boolean isFile() {
         return this.next.indexOf(".") != -1;
     }
 
-    private void setLength(int index, int length) {
-        Integer origin = longest.get(index);
-        if (origin == null) {
-            longest.put(index, length);
-        } else {
-            int l = length > origin ? length : origin;
-            longest.put(index, l);
-        }
-    }
-
     private int findNextN(String input, int index) {
-        next.delete(0, next.length());
+        next.setLength(0);
         tNum = 0;
         while (index >= 0) {
             char c = input.charAt(index);
@@ -102,8 +88,11 @@ public class LongestAbsoluteFilePath {
         return index;
     }
 
-    private int max(Integer a, int b) {
-        int a1 = a == null ? 0 : a;
-        return Math.max(a1, b);
+    private int get(Integer key) {
+        if (longest.get(key) == null) {
+            return 0;
+        } else {
+            return longest.get(key);
+        }
     }
 }
