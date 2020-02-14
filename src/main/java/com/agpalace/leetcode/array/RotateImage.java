32bcfd9d9db3634f1df49c197822815905437017
Ming Deng <mindeng@ebay.com>
2020-02-14 23:49:58 +0800

    Add RotateImage
    Signed-off-by: Ming Deng <mindeng@ebay.com>

--- src/main/java/com/agpalace/leetcode/array/RotateImage.java
+++ src/main/java/com/agpalace/leetcode/array/RotateImage.java
@@ -5,6 +5,16 @@ package com.agpalace.leetcode.array;
  */
 public class RotateImage {
     public void rotate(int[][] matrix) {
-
+        int length = matrix.length;
+        int count = length / 2;
+        for (int i = 0; i < count; i++) {
+            for (int j = i; j < length - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[length - j - 1][i];
+                matrix[length - j - 1][i] = matrix[length - i - 1][length - j - 1];
+                matrix[length - i - 1][length - j - 1] = matrix[j][length - i - 1];
+                matrix[j][length - i - 1] = temp;
+            }
+        }
     }
 }
---
+++ src/main/java/com/agpalace/leetcode/string/ReverseString.java
@@ -0,0 +1,10 @@
+package com.agpalace.leetcode.string;
+
+/**
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+ */
+public class ReverseString {
+    public void reverseString(char[] s) {
+
+    }
+}
