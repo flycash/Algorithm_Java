991c1e300bd9c67622c933b472a648225da1543e
Ming Deng <mindeng@ebay.com>
2020-03-01 22:51:32 +0800

    Add StringToInteger

---
+++ src/main/java/com/agpalace/leetcode/string/StrStr.java
@@ -0,0 +1,8 @@
+package com.agpalace.leetcode.string;
+
+/**
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/885/
+ */
+public class StrStr {
+
+}
--- src/main/java/com/agpalace/leetcode/string/StringToInteger.java
+++ src/main/java/com/agpalace/leetcode/string/StringToInteger.java
@@ -4,4 +4,57 @@ package com.agpalace.leetcode.string;
  * https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/884/
  */
 public class StringToInteger {
+
+    public int myAtoi(String str) {
+        long result = 0;
+        int i = 0;
+
+        boolean positive = true;
+        while (i < str.length() && str.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i == str.length()) {
+            return 0;
+        }
+
+        if (str.charAt(i) == '+') {
+            i++;
+        } else if (str.charAt(i) == '-') {
+            i++;
+            positive = false;
+        }
+
+        if (i == str.length()) {
+            return 0;
+        }
+
+        while (i < str.length()
+                && str.charAt(i) <= '9'
+                && str.charAt(i) >= '0') {
+            result = result * 10 + (str.charAt(i) - '0');
+            if (positive && result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (!positive && result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+
+        if (!positive) {
+            result = -result;
+        }
+
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        return (int) result;
+    }
 }
